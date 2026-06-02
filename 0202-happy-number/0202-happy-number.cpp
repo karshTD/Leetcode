28fdd90b0c7d9a09d3f7ae7436bef21ddee5e257
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-06-02 23:55:10 +0530

    Time: 0 ms (100%), Space: 8.3 MB (38.18%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int nextnumber(int n){
+         int sum = 0;
+        while( n > 0){
+       
+        int digit = n % 10;
+        sum = sum + digit * digit;
+        n = n/10;}
+
+        return sum;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while( n != 1){
+            if( seen.find(n) != seen.end()){
+                return false;
+            }
+            
+            seen.insert(n);
+            n = nextnumber(n);
+        }
+
+    return true;
+    }
+};
